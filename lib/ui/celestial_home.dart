842010e21207b1702c5a5cd4971b117fc6cfde68
Armard Bellamy <armardbellamy@gmail.com>
2018-09-18 18:49:17 -0400

    Add styling and first input field

--- lib/ui/celestial_home.dart
+++ lib/ui/celestial_home.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class CelestialHome extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
+    return new CelestialState();
   }
 
 }
@@ -11,6 +11,43 @@ class CelestialHome extends StatefulWidget {
 class CelestialState extends State<CelestialHome> {
   @override
   Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Colors.black38,
+        title: new Text('Weight On Planet X'),
+          centerTitle: true,
+      ),
+      backgroundColor: Colors.blueGrey,
+      body: new Container(
+        alignment: Alignment.topCenter,
+        child: new ListView(
+          padding: const EdgeInsets.all(2.5),
+          children: <Widget>[
+            new Image.asset("images/planet.png",
+            height: 133.0,
+            width: 200.0,),
+            new Container(
+              margin: const EdgeInsets.all(3.0),
+              alignment: Alignment.center,
+              child: new Column(
+                children: <Widget>[
+                  new TextField(
+                    controller: null,
+                    keyboardType: TextInputType.number,
+                    decoration: new InputDecoration(
+                      labelText: 'Your Weight on Earth',
+                      hintText: 'In pounds',
+                      icon: new Icon(Icons.person_outline)
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+
+    );
 
   }
 
