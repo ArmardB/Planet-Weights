7fc08c9d61f645e1eded4da1fa918ff067c47618
Armard Bellamy <armardbellamy@gmail.com>
2018-09-18 18:25:23 -0400

    Boilerplate and added icon

---
+++ lib/celestial_home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CelestialHome extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+  }
+
+}
+
+class CelestialState extends State<CelestialHome> {
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    title: 'Celestrial Pounds',
+    home: new CelestialHome(),
+  ));
+}
